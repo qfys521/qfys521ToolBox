a878e3e9cfbd4f3f85ac8a649ae7190a63c4e5df
qfys521 <qfys520@vip.qq.com>
2023-01-09 19:22:44 +0800

    new

--- src/main/java/cn/qfys521/xiaoming/xiaoMingEssInteractor.java
+++ src/main/java/cn/qfys521/xiaoming/xiaoMingEssInteractor.java
@@ -8,13 +8,13 @@ import cn.chuanwise.xiaoming.plugin.Plugin;
 import cn.chuanwise.xiaoming.user.XiaoMingUser;
 import cn.qfys521.HttpUtil.getURLData;
 import cn.qfys521.qfys521ToolBoxPlugin;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class xiaoMingEssInteractor extends SimpleInteractors<qfys521ToolBoxPlugin> {
@@ -78,5 +78,59 @@ public class xiaoMingEssInteractor extends SimpleInteractors<qfys521ToolBoxPlugi
         getURLData get = new getURLData();
         String request = get.getUrlData("https://api.oick.cn/yulu/api.php");
         user.sendMessage(request);
+    }
+    @Filter("ICP备案查询 {url}")
+    public void icp(XiaoMingUser user,@FilterParameter("url")String url) throws IOException {
+        getURLData get = new getURLData();
+        String request = get.getUrlData("https://api.oick.cn/icp/api.php?url="+url);
+        JSONObject json = JSONObject.parseObject(request);
+        String code = json.getString("code");
+        String name = json.getString("name");
+        String nature = json.getString("nature");
+        String icp = json.getString("icp");
+        String site_name = json.getString("site_name");
+        String site_index = json.getString("site_index");
+        String site_time = json.getString("site_time");
+        StringBuilder sb = new StringBuilder();
+        if(Objects.equals(code, "201")){
+            user.sendError(json.getString("msg"));
+        }else{
+            user.sendMessage(
+                    "主办单位名称"+code+"\n"+
+                            "主办单位性质"+nature+"\n"+
+                            "网站备案/许可证号"+icp+"\n"+
+                            "网站名称"+site_name+"\n"+
+                            "网站首页网址"+site_index+"\n"+
+                            "审核时间"+site_time);
+        }
+    }
+
+    @Filter("历史上的今天")
+    public void history(XiaoMingUser user) throws IOException{
+        getURLData get = new getURLData();
+        String request = get.getUrlData("https://api.oick.cn/lishi/api.php");
+        JSONObject json = JSONObject.parseObject(request);
+        String code = json.getString("code");
+        String day = json.getString("day");
+        String arr = json.getString("result").replaceAll("\\[","").replaceAll("]","");
+        String[] result = arr.split(",");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0;i<result.length;i++) {
+            JSONObject jsonObject = JSONObject.parseObject(result[i]);
+            String date = jsonObject.getString("date");
+            String title = jsonObject.getString("title");
+            sb
+                    .append(date)
+                    .append(":")
+                    .append(title);
+
+        }
+        if(Objects.equals(code, "-1")){
+            user.sendError(json.getString("msg"));
+        }else{
+            user.sendMessage("今天为:"+day+"\n"+
+                    "历史上的今天:"+sb
+            );
+        }
     }
 }
